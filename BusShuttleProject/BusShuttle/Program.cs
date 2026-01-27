c065a481de0852187058728f81b1ef636d82b61c
s-godbo <samantha.godbold@bsu.edu>
2026-01-27 16:52:42 +0000

    some refactoring to cut responsibilities

--- BusShuttleProject/BusShuttle/Program.cs
+++ BusShuttleProject/BusShuttle/Program.cs
@@ -6,28 +6,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Please select mode (driver OR manager): ");
-        string mode = Console.ReadLine();
-
-        if(mode=="driver") {
-
-            string command;
-
-            do {
-
-                Console.Write("Enter stop name: ");
-                string stopName = Console.ReadLine();
-
-                Console.Write("Enter number of boarded passengers: ");
-                int boarded = int.Parse(Console.ReadLine());
-
-                File.AppendAllText("passenger-data.txt",stopName+":"+boarded+Environment.NewLine);
-
-                Console.Write("Enter command (end OR continue): ");
-                command = Console.ReadLine();
-
-            } while(command!="end");
-
-        }
+        ConsoleUI theUI = new ConsoleUI();
+        theUI.Show();
     }
 }
