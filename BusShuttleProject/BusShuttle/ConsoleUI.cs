eb10670e6826efb842d9d4435fd245ff5abe586a
s-godbo <samantha.godbold@bsu.edu>
2026-01-27 21:51:31 +0000

    manager screen initial

--- BusShuttleProject/BusShuttle/ConsoleUI.cs
+++ BusShuttleProject/BusShuttle/ConsoleUI.cs
@@ -58,10 +58,40 @@ public class ConsoleUI
                         }));
 
             } while (command != "end");
+
+        } else if(mode=="manager") {
+            string command;
+            do {
+                command = AnsiConsole.Prompt(
+                    new SelectionPrompt<string>()
+                        .Title("What do you want to do?")
+                        .AddChoices(new[] {
+                            "add stop", "delete stop", "list all stops", "end"
+                        }));
+
+                if(command=="add stop") {
+                    var newStopName = AnsiConsole.Prompt(new TextPrompt<string>("Enter new stop name:"));
+                    dataManager.AddStop(new Stop(newStopName));
+
+                } else if(command=="delete stop") {
+                    var selectedStop = AnsiConsole.Prompt(new SelectionPrompt<Stop>().Title("Select a stop").AddChoices(dataManager.Stops));
+                    dataManager.RemoveStop(selectedStop);
+
+                } else if(command=="list all stops") {
+                    var table = new Table();
+                    table.AddColumn("Stop Name");
+                    foreach(var stop in dataManager.Stops) {
+                        table.AddRow(stop.Name);
+                    }
+                    AnsiConsole.Write(table);
+                } 
+ 
+            } while(command!="end");
+
+
         }
     }
-    static string AskForInput(string message)
-    {
+    public static string AskForInput(string message) {
         Console.Write(message);
         return Console.ReadLine();
     }
